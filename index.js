9d976d57d802db06f7c55ed032f293fd62c2f873
shravany856 <115397306+shravany856@users.noreply.github.com>
2023-01-08 21:44:40 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-const app =require('./app.js');
-app.listen('3000',()=>{
-  console.log("Listening on port 3000");
-})
+let itemInput = document.getElementById('item-name-input');
+let priceInput = document.getElementById('item-price-input');
+let button = document.getElementById('add');
+
+let table = document.getElementById('table');
+let total = document.getElementById('total');
+
+let priceArr = [];
+function addItem() {
+    let item = itemInput.value;
+    let price = priceInput.value;
+    let sum = 0;
+
+
+    if(item != '' && price!= ''){
+        let row = document.createElement('tr');
+        priceArr.push(parseInt(price));
+        //console.log(priceArr);
+        row.innerHTML = `<td>${item}</td> <td>${price}</td>`
+
+        for(let i=0; i < priceArr.length; i++){
+            sum += priceArr[i];
+        }
+
+        console.log(sum);
+        table.append(row);
+        total.innerText = sum;
+
+        itemInput.value = '';
+        priceInput.value = '';
+    }
+
+
+}
+
+button.addEventListener('click', addItem)
